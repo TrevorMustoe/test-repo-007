eec4ae31e564b356394390ebe0c6b43c18e5c05d
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-04-03 16:53:56 -0400

    Added in new start files and added in a cool james bond button since its reop-007

---
+++ main.js
@@ -0,0 +1,5 @@
+const showImageButton = document.getElementById("show-bond");
+      const myImage = document.getElementById("picture"); 
+      showImageButton.addEventListener("click", () => { 
+         myImage.style.display = "block"; 
+      });
